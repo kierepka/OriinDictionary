4f0d906779978a40e8b466f618c4141c03008926
Mateusz Kierepka <mateusz@kierepka.pl>
2022-09-29 08:13:11 +0200

    update

---
+++ OrinDictionaryTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace OrinDictionaryTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
